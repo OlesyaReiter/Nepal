eb1b456f3b313ac26f7be38ceef1ae5cb1024705
Olesya Raiter <olesya.reiter@gmail.com>
2017-06-15 22:22:33 +0300

    Comments added for merge test

--- src/js/slide.js
+++ src/js/slide.js
@@ -8,9 +8,11 @@ function showSlides() {
     for (i = 0; i < slides.length; i++) {
        slides[i].style.display = "none";  
     }
+    //Merge comment1
     slideIndex++;
     if (slideIndex> slides.length) {slideIndex = 1}    
     for (i = 0; i < dots.length; i++) {
+        //Merge comment2
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display = "block";  
